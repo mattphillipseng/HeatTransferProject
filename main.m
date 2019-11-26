82bc6238d25d8189ea1cee3aa92d68fa90fb042b
Matthew Phillips <matthewp1234@gmail.com>
2019-11-26 00:16:04 -0500

    R cond calculated

--- main.m
+++ main.m
@@ -18,8 +18,8 @@ pipe_od = pipe_id_inch * 0.0254; % outer diam in [m]
 
 length = 80; % length of pipe [m]
 
-%% Thermal Properties
-
+%% Material/Fluid Properties
+k_ss = 19; % [W/m*K] Thermal conductivity: 304 stainless steel (18% Cr, 8% Ni) at 300 deg C
 
 %% Internal Flow Parameters and Constants
 m_dot_per_hr = 6000; % [Kg/h]
@@ -31,4 +31,12 @@ T_steam = 324.6675 + 273.15; % [K] interpolated from thermo tables at 12MPa=1200
 wind_highest = 25; % Highest wind speed [m/s]
 T_coldest = -30 + 273.15; % Coldest outside temp [K]
 
+%% Thermal Circuit of Un-Insulated Pipe
+% T_b = Bulk temperature T_steam everywhere
+% T_wi = T wall inner = T_steam everywhere because h inside is very large
+% T_wo = T wall outer
+% T_inf = T of cold wind = T_coldest
+% T_surr = T of surroundings = T_coldest
+
 %% Calculations
+R_cond = log(pipe_od/pipe_id) / (2*pi*length*k_ss); % R through pipe wall
